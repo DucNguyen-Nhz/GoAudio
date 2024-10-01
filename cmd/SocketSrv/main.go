37b87666e6a71be42f2f64c3819928a99b670a45
Nahzilian <nahz3112@gmail.com>
2024-09-30 20:07:54 -0400

    Update

--- cmd/SocketSrv/main.go
+++ cmd/SocketSrv/main.go
@@ -4,16 +4,18 @@ import (
 	"fmt"
 	"net/http"
 
-	sck "github.com/DucNguyen-Nhz/GoStream/internal/socket"
+	"os"
 
+	sck "github.com/DucNguyen-Nhz/GoStream/internal/socket"
 	"golang.org/x/net/websocket"
 )
 
-var (
-	port = fmt.Sprint(3000)
-)
-
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
 	server := sck.CreateWS()
 	fmt.Println("Server started on port " + port)
 	http.Handle("/ws", websocket.Handler(server.HandleWS))
